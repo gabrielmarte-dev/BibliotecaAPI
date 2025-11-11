31200b81329c3ff91cde447175ed14dd85443415
gabrielmarte-dev <gmarte303@gmail.com>
2025-11-11 00:51:04 -0400

    Agregar configuración de Swagger para la documentación de la API

--- Program.cs
+++ Program.cs
@@ -9,7 +9,16 @@ builder.Services.AddControllers().AddJsonOptions(opciones => opciones.JsonSerial
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Area de middlewares
 app.MapControllers();
